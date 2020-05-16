5ab82736b85160471917b47cccacf14bc5d97012
whouohm <65443549+whouohm@users.noreply.github.com>
2020-05-16 16:11:31 +0700

    Update line.php

--- line.php
+++ line.php
@@ -14,7 +14,7 @@ function send_LINE($msg){
       $url = 'https://api.line.me/v2/bot/message/push';
       $data = [
 
-        'to' => 'Ue77a191627f6ac91899e75d92264310c',
+        'userid' => 'Ue77a191627f6ac91899e75d92264310c',
         'messages' => [$messages],
       ];
       $post = json_encode($data);
