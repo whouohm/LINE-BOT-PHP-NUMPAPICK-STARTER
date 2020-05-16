f235245fcb9fe19b985f4c26a94cc0718f00b34d
whouohm <65443549+whouohm@users.noreply.github.com>
2020-05-16 16:07:56 +0700

    Update pub.php

--- pub.php
+++ pub.php
@@ -1,8 +1,8 @@
  <?php
  function pubMqtt($topic,$msg){
-       $APPID= "numpapicklinebot/"; //enter your appid
-     $KEY = "mJ7K4MfteC7p0dW"; //enter your key
-    $SECRET = "pp4gzMhCvJIqlxc66hKEvk46m"; //enter your secret
+       $APPID= "whouohm/"; //enter your appid
+     $KEY = "ynmN6QVpJzpqxLl"; //enter your key
+    $SECRET = "1gqLjj70CmAX2OFWpXv4HuxDr"; //enter your secret
     $Topic = "$topic"; 
       put("https://api.netpie.io/microgear/".$APPID.$Topic."?retain&auth=".$KEY.":".$SECRET,$msg);
  
